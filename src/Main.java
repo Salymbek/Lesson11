3e22fd4e8fec75433537bcbb5ef9c68bbcb04cd8
Salymbek <calumbekcaatbekov@gmail.com>
2022-11-01 15:14:28 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Triangle triangle = new Triangle();
+        Scanner scanner = new Scanner(System.in);
+        triangle.a = scanner.nextInt();
+        triangle.b = scanner.nextInt();
+        triangle.c = scanner.nextInt();
+
+        triangle.area(triangle.a, triangle.b, triangle.c);
+
+
+
+
+
     }
+
 }
